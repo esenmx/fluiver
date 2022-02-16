cc858ee40832d3a866bfd279158cc8795e4bb232
softronaut <mathronaut@gmail.com>
2022-02-16 12:49:02 +0300

    iterable_num tests, test conventions

---
+++ test/iterable_num_test.dart
@@ -0,0 +1,23 @@
+import 'package:dashx/dashx.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() async {
+  group('int', () {
+    test('sum', () {
+      expect(<int>[].sum, 0);
+      expect([1].sum, 1);
+      expect([1, 2, 3].sum, 6);
+      expect(List.generate(100, (i) => i).sum, 99 * (99 + 1) / 2);
+    });
+  });
+
+  group('double', () {
+    test('sum', () {
+      expect(<double>[].sum, 0.0);
+      expect([1.1].sum, 1.1);
+      expect([1.1, 2.2, 3.3].sum, 6.6);
+      expect(List<double>.generate(100, (i) => i.toDouble()).sum,
+          (99 * (99 + 1) / 2).toDouble());
+    });
+  });
+}
