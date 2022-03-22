ab503cec3994443050971284428a4555b0c16c66
softronaut <mathronaut@gmail.com>
2022-03-22 20:44:55 +0300

    padded_flex.dart added

---
+++ lib/src/widgets/padded_flex.dart
@@ -0,0 +1,55 @@
+part of dashx;
+
+class PaddedColumn extends Padding {
+  PaddedColumn({
+    Key? key,
+    required EdgeInsetsGeometry padding,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+    List<Widget> children = const <Widget>[],
+  }) : super(
+          key: key,
+          padding: padding,
+          child: Column(
+            key: key,
+            mainAxisAlignment: mainAxisAlignment,
+            mainAxisSize: mainAxisSize,
+            crossAxisAlignment: crossAxisAlignment,
+            textDirection: textDirection,
+            verticalDirection: verticalDirection,
+            textBaseline: textBaseline,
+            children: children,
+          ),
+        );
+}
+
+class PaddedRow extends Padding {
+  PaddedRow({
+    Key? key,
+    required EdgeInsetsGeometry padding,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+    List<Widget> children = const <Widget>[],
+  }) : super(
+          key: key,
+          padding: padding,
+          child: Row(
+            key: key,
+            mainAxisAlignment: mainAxisAlignment,
+            mainAxisSize: mainAxisSize,
+            crossAxisAlignment: crossAxisAlignment,
+            textDirection: textDirection,
+            verticalDirection: verticalDirection,
+            textBaseline: textBaseline,
+            children: children,
+          ),
+        );
+}
