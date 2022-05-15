cd31996689f268e26c966c606479afd08faae069
esenmx <mehmetesen@protonmail.com>
2022-05-15 21:46:18 +0300

    StaggeredStack improvements

--- lib/src/widgets/stack.dart
+++ lib/src/widgets/stack.dart
@@ -2,26 +2,39 @@ part of fluiver;
 
 /// Works well with [NestedScrollView]
 /// Constructs a [Stack] that behaves like [Column]
-/// My usualy usecase is collapsing [AppBar]s with [SliverPersistentHeaderDelegate]s
-/// where centering the middle and transforming into collapsed title needed
+/// My usual usecase is collapsing [AppBar] with [SliverPersistentHeaderDelegate]
+/// where centering the middle and transforming into compressed title needed
 class StaggeredStack extends StatelessWidget {
   StaggeredStack({
     Key? key,
     required this.children,
     this.background,
     this.defaultHeight,
+    this.defaultWidth,
     this.defaultGap,
     this.topOffset = 0,
+
+    /// Stack parameters
+    this.alignment = Alignment.center,
+    this.fit = StackFit.loose,
+    this.clipBehaviour = Clip.hardEdge,
   })  : assert(children.every((e) =>
             (e.height != null || defaultHeight != null) &&
-            (e.gap != null || defaultGap != null))),
+            (e.bottomPadding != null || defaultGap != null) &&
+            (e.width != null || defaultWidth != null))),
         super(key: key);
 
   final Iterable<StaggeredStackChild> children;
   final Widget? background;
+  final double? defaultWidth;
   final double? defaultHeight;
   final double? defaultGap;
   final double topOffset;
+
+  /// Stack parameters
+  final Alignment alignment;
+  final StackFit fit;
+  final Clip clipBehaviour;
 
   @override
   Widget build(BuildContext context) {
@@ -29,27 +42,36 @@ class StaggeredStack extends StatelessWidget {
     for (var i = 0; i < children.length; i++) {
       final e = children.elementAt(i);
       final top = children.take(i).fold<double>(0, (pv, e) {
-        return pv + (e.gap ?? defaultGap!) + (e.height ?? defaultHeight!);
+        return pv +
+            (e.bottomPadding ?? defaultGap!) +
+            (e.height ?? defaultHeight!);
       });
       c.add(Positioned(
-        child: e.child,
         height: e.height ?? defaultHeight,
+        width: e.width ?? defaultWidth,
         top: topOffset + top,
+        child: e.child,
       ));
     }
     return Stack(
-      alignment: Alignment.center,
-      clipBehavior: Clip.antiAlias,
-      fit: StackFit.expand,
+      alignment: alignment,
       children: [if (background != null) background!, ...c],
+      fit: fit,
+      clipBehavior: clipBehaviour,
     );
   }
 }
 
 class StaggeredStackChild {
-  StaggeredStackChild({required this.child, this.height, this.gap});
+  StaggeredStackChild({
+    required this.child,
+    this.width,
+    this.height,
+    this.bottomPadding,
+  });
 
   final Widget child;
+  final double? width;
   final double? height;
-  final double? gap;
+  final double? bottomPadding;
 }
